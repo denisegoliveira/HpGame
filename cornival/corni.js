406582ad953b9174e1f39629f1b110c5acc64221
denisegoliveira <goliveira.denise@gmail.com>
2021-11-26 19:06:06 -0300

    deixar site responsivo

--- cornival/corni.js
+++ cornival/corni.js
@@ -1,16 +1,23 @@
 function etapasCornival1(opcao) {
-    var confirmacao = prompt("Para confirmar sua resposta digite\n1 para sim\n2 para não")
+    
+    var validaEscolha = false;
+
+    while(validaEscolha == false) {
+    var confirmacao = prompt("Para confirmar sua resposta digite 1")
 
     if (opcao == 1 && confirmacao == 1) {
+        validaEscolha = true
         return location = '../../game_over/gameover.html'
     } else if (opcao == 2 && confirmacao == 1) {
+        validaEscolha = true
         return location = '../../game_over/gameover.html'
     } else if (opcao == 3 && confirmacao == 1) {
+        validaEscolha = true
         return location = '../corni_p2/corni_p2.html'
     } else {
         alert ("Você não confirmou sua resposta")
-        return location = './corni_p1/corni_p1.html'
     }
+}
 }
 
 function etapasCornival2(opcao) {
--- pagina_escolha/pagina.js
+++ pagina_escolha/pagina.js
@@ -1,17 +1,28 @@
 
 function escolherPersonagem(nomePersonagem) {
 
+var validaEscolha = false;
+while(validaEscolha == false) {
+
 var quiz = prompt('Digite 1 para continuar \n Digite 2 para escolher outro personagem');
 
 if (nomePersonagem == "Neville" && quiz == 1) {
+    validaEscolha = true
     return location = '../grifinoria/grifi_p1/p1.html'
 } else if (nomePersonagem == "Luna" && quiz == 1) {
+    validaEscolha = true
    return location = '../cornival/corni_p1/corni_p1.html'
 } else if (nomePersonagem == "Cedrico" && quiz == 1) {
+    validaEscolha = true
     return location = '../lufalufa/lufa_p1/lufa_p1.html'
 } else if (nomePersonagem == "Draco" && quiz == 1) {
+    validaEscolha = true
     return location = '../sanserina/sonse_p1/sonse_p1.html'
-} else {
+} else if (quiz == 2) {
+    validaEscolha = true
     return location = './index.html'
+} else {
+    alert ("opção inválida")
+}
 }
 }
